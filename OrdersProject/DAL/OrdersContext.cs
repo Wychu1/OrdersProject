32a751cc3945fb084633c7bc6edfb18fb974057a
Wojtek <wychu69@gmail.com>
2017-05-25 19:47:15 +0200

    Start

---
+++ OrdersProject/DAL/OrdersContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using OrdersProject.Views.Entities;
+
+namespace OrdersProject.DAL
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext() : base("OrdersContext")
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ OrdersProject/Views/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrdersProject.Views.Entities
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+
+    }
+}
