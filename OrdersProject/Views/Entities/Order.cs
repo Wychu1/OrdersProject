7190dc6e0e72a7a08e724aae368253eb2d961281
Wojtek <wychu69@gmail.com>
2017-05-25 20:50:37 +0200

    ..

--- OrdersProject/Views/Entities/Order.cs
+++ OrdersProject/Views/Entities/Order.cs
@@ -5,10 +5,23 @@ using System.Web;
 
 namespace OrdersProject.Views.Entities
 {
+    public enum Agency
+    {
+        Egp,
+        Tbwa,
+        Tbwapr,
+        Tequila
+
+    }
     public class Order
     {
         public long Id { get; set; }
-        public string name { get; set; }
+        public Agency Agency { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public bool PaymentStatus { get; set; }
+        public string Seller { get; set; }
+        public string Buyer { get; set; }
 
     }
 }
--- packages/Castle.Windsor.3.3.0/readme.txt
+++ packages/Castle.Windsor.3.3.0/readme.txt
@@ -1,7 +1,7 @@
 Thanks for downloading this Castle Windsor package.
 You can find full list of changes in changes.txt
 
-Documentation:		- http://www.castleproject.org/projects/windsor/
+Documentation:		- http://docs.castleproject.org/Windsor.MainPage.ashx
 Discusssion group: 	- http://groups.google.com/group/castle-project-users
 StackOverflow tags:	- castle-windsor, castle
 
